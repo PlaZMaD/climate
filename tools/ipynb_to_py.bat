ff4f62fd27faa13245971afe37ed73a50e03fa97
halt9k <49090368+halt9k@users.noreply.github.com>
2025-08-09 00:20:34 +0300

    ﻿wip: verifications

--- tools/ipynb_to_py.bat
+++ tools/ipynb_to_py.bat
@@ -1,2 +1,6 @@
 @echo off
-%CONDA_ENVS%\ipynb-3-11-13\python.exe -m jupytext --to py ..\FluxFilter.ipynb
+%CONDA_ENVS%\ipynb-3-11-13\python.exe -m jupytext --to py ..\FluxFilter*.ipynb
+
+@echo FluxFilter*.ipynb will be deleted...
+pause
+del ..\FluxFilter*.ipynb
