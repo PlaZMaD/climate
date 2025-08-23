9d3fbbe68c6de72741c2a6675f04c5233e231da4
halt9k <49090368+halt9k@users.noreply.github.com>
2025-08-23 11:41:13 +0300

    test: repairs

--- src/cells_mirror/cell_reddyproc_draw.py
+++ src/cells_mirror/cell_reddyproc_draw.py
@@ -1,41 +1,41 @@
 # Reminder: this is duplicate of specific cell used for test purposes, it is outdated or ahead frequently
-
 from pathlib import Path
 
-import src.ipynb_globals as ig
+from src.ipynb_globals import *
 from src.colab_routines import colab_add_download_button
 from src.helpers.io_helpers import create_archive
-from src.reddyproc.postprocess_graphs import RepOutputHandler, RepImgTagHandler, RepOutputGen
+from src.reddyproc.postprocess_graphs import RepImgTagHandler, RepOutputGen, RepOutputHandler
 
-rep_out_dir = Path(ig.rep.options.output_dir)
-tag_handler = RepImgTagHandler(main_path=rep_out_dir, rep_cfg=ig.rep, img_ext='.png')
-eog = RepOutputGen(tag_handler)
+rep_out_dir = Path(config.reddyproc.output_dir)
+tag_handler = RepImgTagHandler(main_path=rep_out_dir, rep_cfg=config.reddyproc, rep_out_info=gl.rep_out_info,
+                               img_ext='.png')
+rog = RepOutputGen(tag_handler)
 
-output_sequence: tuple[list[str] | str, ...] = (
+output_sequence: tuple[str | list[str], ...] = (
     "## Тепловые карты",
-    eog.hmap_compare_row('NEE_*'),
-    eog.hmap_compare_row('LE_f'),
-    eog.hmap_compare_row('H_f'),
+    rog.hmap_compare_row('NEE_*'),
+    rog.hmap_compare_row('LE_f'),
+    rog.hmap_compare_row('H_f'),
     "## Суточный ход",
-    eog.diurnal_cycle_row('NEE_*'),
-    eog.diurnal_cycle_row('LE_f'),
-    eog.diurnal_cycle_row('H_f'),
+    rog.diurnal_cycle_row('NEE_*'),
+    rog.diurnal_cycle_row('LE_f'),
+    rog.diurnal_cycle_row('H_f'),
     "## 30-минутные потоки и суточные средние",
-    eog.flux_compare_row('NEE_*'),
-    eog.flux_compare_row('LE_f'),
-    eog.flux_compare_row('H_f')
+    rog.flux_compare_row('NEE_*'),
+    rog.flux_compare_row('LE_f'),
+    rog.flux_compare_row('H_f')
 )
 
-eio = RepOutputHandler(output_sequence=output_sequence, tag_handler=tag_handler, out_info=ig.rep.out_info)
-eio.prepare_images_safe()
-ig.arc_exclude_files = eio.img_proc.raw_img_duplicates
+roh = RepOutputHandler(output_sequence=output_sequence, tag_handler=tag_handler, out_info=gl.rep_out_info)
+roh.prepare_images_safe()
+gl.rep_arc_exclude_files = roh.img_proc.raw_img_duplicates
 
-eproc_arc_path = rep_out_dir / (ig.rep.out_info.fnames_prefix + '.zip')
-create_archive(arc_path=eproc_arc_path, folders=rep_out_dir, top_folder=rep_out_dir,
-               include_fmasks=['*.png', '*.csv', '*.txt'], exclude_files=eio.img_proc.raw_img_duplicates)
+rep_arc_path = rep_out_dir / (gl.rep_out_info.fnames_prefix + '.zip')
+create_archive(arc_path=rep_arc_path, folders=rep_out_dir, top_folder=rep_out_dir,
+               include_fmasks=['*.png', '*.csv', '*.txt'], exclude_files=roh.img_proc.raw_img_duplicates)
 
-colab_add_download_button(eproc_arc_path, 'Download eddyproc outputs')
+colab_add_download_button(rep_arc_path, 'Download eddyproc outputs')
 
-eio.display_images_safe()
+roh.display_images_safe()
 
-tag_handler.display_tag_info(eio.extended_tags())
+tag_handler.display_tag_info(roh.extended_tags())
--- test/reddyproc/test_reddyproc_py.py
+++ test/reddyproc/test_reddyproc_py.py
@@ -1,9 +1,11 @@
+from pathlib import Path
 from types import SimpleNamespace
 
 import pytest
 
 import src.helpers.os_helpers  # noqa: F401
 import src.ipynb_globals as ig
+from src.ffconfig import FFConfig, RepOutInfo, FFGlobals, RepConfig
 from src.helpers.env_helpers import setup_r_env
 from test.reddyproc.helpers.io_helpers import find_rep_file
 
@@ -20,25 +22,33 @@ def use_r_from_python_env():
     # robjects.r("install.packages('https://cran.r-project.org/bin/windows/contrib/4.2/REddyProc_1.3.3.zip', repos = NULL, type = 'binary')")
 
 
-
 def test_process(use_r_from_python_env):
-    # ig.ias_output_prefix = 'kr_tur'
-    # ig.reddyproc_filename = 'REddyProc.txt'
     rep_input_file = find_rep_file('output/*REddyProc*.txt')
-    ig.rep_input_fpath = rep_input_file.fpath
-    ig.ias_output_prefix = rep_input_file.site_id
+
+    ig.gl = FFGlobals(
+        rep_level3_fpath=rep_input_file.fpath,
+        out_dir=Path('output'),
+    )
+
+    ig.config = FFConfig(
+        version='0.0.0',
+        site_name=rep_input_file.site_id
+    )
 
     import src.cells_mirror.cell_reddyproc_process  # noqa: F401
     # import src.cells_mirror.cell_reddyproc_draw  # noqa: F401
 
 
 def test_draw():
-    ig.rep = SimpleNamespace()
-    ig.rep.out_info = SimpleNamespace()
-    ig.rep.options = SimpleNamespace(is_to_apply_u_star_filtering=True)
-    ig.rep.out_info.fnames_prefix = 'tv_fy4_2023'
-    ig.rep.out_info.start_year = 2023
-    ig.rep.out_info.end_year = 2023
+    ig.config = FFConfig(
+        version='0.0.0',
+        reddyproc=RepConfig(is_to_apply_u_star_filtering=True)
+    )
+
+    ig.gl = FFGlobals(
+        out_dir=Path('output'),
+        rep_out_info=RepOutInfo(fnames_prefix='tv_fy4_2023', start_year=2023, end_year=2023)
+    )
     # ensure_empty_dir('output/reddyproc')
     # shutil.copytree('test/reddyproc/test_process/output_sample', 'output/reddyproc', dirs_exist_ok=True)
     import src.cells_mirror.cell_reddyproc_draw  # noqa: F401
