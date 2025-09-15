c82ce71b2c92a99546fc23de0566cc3cd99bcb80
halt9k <49090368+halt9k@users.noreply.github.com>
2025-09-15 17:23:01 +0300

    ﻿release: tag

--- FluxFilter.py
+++ FluxFilter.py
@@ -109,7 +109,7 @@
 # !rm -rf sample_data
 # !rm -rf scripts
 
-# %env clone_br=main
+# %env clone_br=v1.0.3
 # %env clone_repo=https://github.com/PlaZMaD/climate.git
 # !git clone -b $clone_br -n --depth=1 --filter=tree:0 $clone_repo scripts
 # !git -C scripts sparse-checkout set --no-cone src locale misc
